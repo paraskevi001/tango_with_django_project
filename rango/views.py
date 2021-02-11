f15c754043dd0f7f30e7d6e5969979032ad6a104
Paraskevi Katalanou <2458764K@student.gla.ac.uk>
2021-02-11 04:17:04 +0200

    Chapt 10

--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,7 @@ from django.template import loader
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from datetime import datetime
 
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
@@ -19,24 +20,34 @@ def index(request):
 	# Retrieve the top 5 only -- or all if less than 5.
 	# Place the list in our context_dict dictionary (with our boldmessage!)
 	# that will be passed to the template engine.
-	category_list = Category.objects.order_by('-likes')[:6]
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
 	
 	# Construct a dictionary to pass to the template engine as its context.
 	# Note the key boldmessage matches to {{ boldmessage }} in the template!
 	context_dict={}
 	context_dict ['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories']=category_list
-	context_dict['pages']=Page.objects.order_by('-views')[:5]
+	context_dict['pages'] = page_list
 	
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier.
-	# Note that the first parameter is the template we wish to use.
-	return render(request, 'rango/index.html', context=context_dict)
+	visitor_cookie_handler(request)
+	
+	
+	response = render(request, 'rango/index.html', context=context_dict)
+	
+	return response
 
 def about(request):
 	print(request.method)
 	print(request.user)
-	return render(request, 'rango/about.html',{})
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
+	visitor_cookie_handler(request)
+	context_dict={}
+	context_dict['visits'] = request.session['visits']
+	
+	return render(request, 'rango/about.html',context=context_dict)
 	
 def show_category(request, category_name_slug):
 	# Create a context dictionary which we can pass
@@ -174,6 +185,27 @@ def restricted(request):
 def user_logout(request):
 	logout(request)
 	return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request,cookie,default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	
+	last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+	last_visit_time=datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+	
+	if (datetime.now() - last_visit_time).days>0:
+		visits=visits+1
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		request.session['last_visit'] = last_visit_cookie
+	
+	request.session['visits'] = visits
+
 
 class Index2View(generic.ListView):
 	template_name='rango/index2.html'
